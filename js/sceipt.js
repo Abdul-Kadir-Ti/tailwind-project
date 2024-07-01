a72fba8b4c93c638600924b7cf95f9431fe3afbf
Abdul Kadir <abdulkadir112k@gmail.com>
2024-07-01 23:59:38 +0600

    Add files via upload

---
+++ js/sceipt.js
@@ -0,0 +1,33 @@
+$('.bnnerSlider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay:true,
+    arrows: false,
+  });
+$('.menuSlider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay:true,
+    arrows: false,
+  });
+
+  
+$('.photoSlider').slick({
+  dots: false,
+  arrows: true,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  centerMode: true,
+  centerPadding: '60px',
+  autoplay:false,
+  prevArrow: `<i class="fa-solid fa-angle-left left"></i>`,
+  nextArrow: `<i class="fa-solid fa-angle-right right"></i>`,
+})
+
